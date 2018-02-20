3805a8d97e94e2359a61626a5a021721cedd7fc5
DonnyStout <donnystout47@gmail.com>
2018-02-20 09:11:32 -0700

    Added a new constructor overload and generate overload

--- src/edu/cnm/deepdive/security/Diceware.java
+++ src/edu/cnm/deepdive/security/Diceware.java
@@ -19,33 +19,37 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- *  The <code>Diceware</code> class implements a Diceware-based passphrase
- *  generator, using a word list provided by invoking a word list in the constructor.
- *  If a psuedo-random number generator is not set (using {@link
- *  Diceware#setRng(Random)} method) then an instance of {@link SecureRandom}
- *  is created and used for selecting words at random from the list.
- *  
+ * The <code>Diceware</code> class implements a Diceware-based passphrase generator, using a word
+ * list provided by invoking a word list in the constructor. If a psuedo-random number generator is
+ * not set (using {@link Diceware#setRng(Random)} method) then an instance of {@link SecureRandom}
+ * is created and used for selecting words at random from the list.
+ * 
  * @author D Stout with Deep Dive Coding Java Cohort 3
- * @version 0.1
+ * @version 0.2
  */
 public class Diceware {
 
+  private static final String DEFAULT_RESOURCE_BUNDLE = "wordlist";
   private static final String NEGATIVE_PASSPHRASE_MESSAGE = "Passphrase length must be positive!";
-
   private static final String LINE_PATTERN = "^\\s*(\\d+)\\s+(\\S+)\\s*$";
 
   private List<String> words;
   private Random rng = null;
-  
+
+
+  public Diceware() {
+    this(ResourceBundle.getBundle(DEFAULT_RESOURCE_BUNDLE));
+  }
+
 
   /**
-   * Initializes an instance of <code>Diceware</code> using a reference to a
-   * {@link java.io.File} object. If the <code>File</code> does not exist
-   * or cannot be read, and exception will be thrown.
+   * Initializes an instance of <code>Diceware</code> using a reference to a {@link java.io.File}
+   * object. If the <code>File</code> does not exist or cannot be read, and exception will be
+   * thrown.
    * 
-   * @param file                     file to read for word list.
-   * @throws FileNotFoundException   if file does not exist.
-   * @throws IOException             if file can't be read.
+   * @param file file to read for word list.
+   * @throws FileNotFoundException if file does not exist.
+   * @throws IOException if file can't be read.
    */
   public Diceware(File file) throws FileNotFoundException, IOException {
     words = new ArrayList<>();
@@ -61,24 +65,23 @@ public class Diceware {
       }
     }
   }
-  
+
   /**
-   *  Initializes an instance of <code>Diceware</code> using a
-   * {@link Collection String} object as the source of words for the word list.
+   * Initializes an instance of <code>Diceware</code> using a {@link Collection String} object as
+   * the source of words for the word list.
    * 
-   * @param source                     word list source.
+   * @param source word list source.
    */
   public Diceware(Collection<String> source) {
     words = new ArrayList<>(source);
   }
 
   /**
-   * Initializes an instance of <code>Diceware</code> using a
-   * {@link ResourceBundle} object as the source of words for the word list.
-   * (The property values from the <code>ResourceBundle</code> are taken as
-   * the words; the property names/keys are ignored.)
+   * Initializes an instance of <code>Diceware</code> using a {@link ResourceBundle} object as the
+   * source of words for the word list. (The property values from the <code>ResourceBundle</code>
+   * are taken as the words; the property names/keys are ignored.)
    * 
-   * @param bundle                     bundles the words in the list provided
+   * @param bundle bundles the words in the list provided
    */
   public Diceware(ResourceBundle bundle) {
     words = new ArrayList<>();
@@ -87,14 +90,14 @@ public class Diceware {
       words.add(bundle.getString(en.nextElement()));
     }
   }
-  
+
   /**
-   * Initializes <code>SecureRandom</code> if random number generator
-   * has not been initialized already.
+   * Initializes <code>SecureRandom</code> if random number generator has not been initialized
+   * already.
    * 
-   * @return                           psudeo-random number generator instance.
-   * @throws NoSuchAlgorithmException  if lazy initialization is used, and default
-   *                                   strong provider algorithm.
+   * @return psudeo-random number generator instance.
+   * @throws NoSuchAlgorithmException if lazy initialization is used, and default strong provider
+   *         algorithm.
    */
   public Random getRng() throws NoSuchAlgorithmException {
     if (rng == null) {
@@ -105,37 +108,35 @@ public class Diceware {
 
   /**
    * Sets the <code>rng</code> for the field.
-   *  
-   * @param rng                        psudeo-random number generator instance
+   * 
+   * @param rng psudeo-random number generator instance
    */
   public void setRng(Random rng) {
     this.rng = rng;
   }
 
   /**
-   * Generates and returns returns (in a <code>String[]</code>) a password of the
-   * specified length. The inclusion of duplicates is controlled by the
-   * <code>duplicatesAllowed</code> argument. If the specified length is
-   * greater than the number of words in the word list, and duplicates aren't
-   * allowed then an infinite loop will result.
+   * Generates and returns returns (in a <code>String[]</code>) a password of the specified length.
+   * The inclusion of duplicates is controlled by the <code>duplicatesAllowed</code> argument. If
+   * the specified length is greater than the number of words in the word list, and duplicates
+   * aren't allowed then an infinite loop will result.
    * 
-   * @param length                     number of words to include in generated passphrase.
-   * @param duplicatesAllowed          if true will allow duplicates of words.
-   * @return                           words in generated passphrase.
-   * @throws NoSuchAlgorithmException  if algorithm for randomness not allowed.
+   * @param length number of words to include in generated passphrase.
+   * @param duplicatesAllowed if true will allow duplicates of words.
+   * @return words in generated passphrase.
+   * @throws NoSuchAlgorithmException if algorithm for randomness not allowed.
    * 
-   * @throws InsufficientPoolException  if password length exceeds word list, and duplicates
-   *                                    not allowed or word list has no words.
+   * @throws InsufficientPoolException if password length exceeds word list, and duplicates not
+   *         allowed or word list has no words.
    * 
-   * @throws IllegalArgumentException  if requested length is negative.
+   * @throws IllegalArgumentException if requested length is negative.
    */
-  public String[] generate(int length, boolean duplicatesAllowed) 
+  public String[] generate(int length, boolean duplicatesAllowed)
       throws NoSuchAlgorithmException, InsufficientPoolException, IllegalArgumentException {
     if (length <= 0) {
       throw new IllegalArgumentException(NEGATIVE_PASSPHRASE_MESSAGE);
     }
-    if ((words.size() == 0 && length > 0) 
-        || (!duplicatesAllowed && length > words.size())) {
+    if ((words.size() == 0 && length > 0) || (!duplicatesAllowed && length > words.size())) {
       throw new InsufficientPoolException();
     }
     List<String> passphrase = new LinkedList<>();
@@ -148,28 +149,34 @@ public class Diceware {
     return passphrase.toArray(new String[passphrase.size()]);
   }
 
+
+  public String generate(int length, String delimiter)
+      throws NoSuchAlgorithmException, InsufficientPoolException, IllegalArgumentException {
+    return generate(length, delimiter, true);
+  }
+
+
   /**
-   * Generates and returns (in a <code>String[]</code>) a password of the
-   * specified length. This method invokes {@link
-   * #generate(int, boolean) generate (length, true)} &ndash; that is, it
+   * Generates and returns (in a <code>String[]</code>) a password of the specified length. This
+   * method invokes {@link #generate(int, boolean) generate (length, true)} &ndash; that is, it
    * invokes {@link #generate(int, boolean)}, specifying that duplicates are allowed.
    * 
-   * @param length                      number of words to include in generated passphrase.
-   * @return                            words in a generated passphrase.
-   * @throws NoSuchAlgorithmException   if algorithm randomness not allowed.
+   * @param length number of words to include in generated passphrase.
+   * @return words in a generated passphrase.
+   * @throws NoSuchAlgorithmException if algorithm randomness not allowed.
    * 
-   * @throws InsufficientPoolException  if word list has no words.
+   * @throws InsufficientPoolException if word list has no words.
    * 
-   * @throws IllegalArgumentException  if requested length issn't positive.
+   * @throws IllegalArgumentException if requested length issn't positive.
    */
-  public String[] generate(int length) 
+  public String[] generate(int length)
       throws NoSuchAlgorithmException, InsufficientPoolException, IllegalArgumentException {
     return generate(length, true);
   }
-  
-  public String generate(int length, String delimiter) 
+
+  public String generate(int length, String delimiter, boolean duplicatesAllowed)
       throws NoSuchAlgorithmException, InsufficientPoolException, IllegalArgumentException {
-    String[] words = generate(length);
+    String[] words = generate(length, duplicatesAllowed);
     StringBuilder builder = new StringBuilder(words[0]);
     for (int i = 1; i < words.length; i++) {
       builder.append(delimiter);
@@ -177,16 +184,16 @@ public class Diceware {
     }
     return builder.toString();
   }
-  
+
   private String generate() throws NoSuchAlgorithmException {
     int index = getRng().nextInt(words.size());
     return words.get(index);
   }
-  
+
   public static class InsufficientPoolException extends IllegalArgumentException {
-    
+
     private InsufficientPoolException() {
-      
+
     }
   }
 
